17ea42036b46073acf09842833a7a091bfac7a6e
eabr7 <e.bruins@protonmail.com>
2025-09-24 18:14:01 +0200

    Did step 6.

--- Backend-java-supermarkt/src/Customer.java
+++ Backend-java-supermarkt/src/Customer.java
@@ -6,12 +6,30 @@ public class Customer {
         this.name = name;
     }
 
-    public void goToSuperMarket (SuperMarket superMarket) {
+    public void goToSuperMarket(SuperMarket superMarket) {
         this.superMarket = superMarket;
     }
 
+    public void buyItem(String productName, int amount) {
+        String bread = "bread";
+        String fruit = "fruit";
+        String cheese = "cheese";
+        String toiletPaper = "toiletPaper";
 
+        if (productName == bread) {
+            superMarket.buyBread(amount);
+        } else if (productName == fruit) {
+            superMarket.buyFruit(amount);
+        } else if (productName == cheese) {
+            superMarket.buyCheese(amount);
+        } else if (productName == toiletPaper) {
+            superMarket.buyToiletPaper(amount);
+        }
+
+    }
 }
+
+
 
 
 
